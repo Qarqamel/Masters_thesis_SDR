393e7b3bfdddf52704cdc7c4341bb67768151bb0
Adam Mundala <adam.mundala@gmail.com>
2023-05-22 04:41:59 +0200

    Changes in AM OOK scripts

--- examples_py/AM_OOK_demodulation.py
+++ examples_py/AM_OOK_demodulation.py
@@ -1,49 +1,66 @@
 import numpy as np
+import scipy.signal
 from matplotlib import pyplot as plt
-from numpy import sin, absolute
-from numpy import pi as PI
-from scipy.signal import butter, filtfilt
 
 def lpf(samples, cutoff_freq, sampling_freq):
-    b, a = butter(2, 2*cutoff_freq/sampling_freq, btype='lowpass')
-    return filtfilt(b,a,samples)
+    b, a = scipy.signal.butter(2, 2*cutoff_freq/sampling_freq, btype='lowpass')
+    return scipy.signal.filtfilt(b,a,samples)
 
-# modulation    
-PAYLOAD                 = [0,1,0,1,1,0,0,1,0,1,1,1]
-SAMPLES_PER_BIT         = 1000
+TIME                        = 6
+SAMPLING_FREQUENCY          = 1000
+FILTER_CUTOFF_FREQUENCY     = 5
+ADC_THRESHOLD               = 0.5
+time = np.linspace(0, TIME, TIME*SAMPLING_FREQUENCY)
+rx = np.load('AM_OOK_TxSignal.npy')
+rectified = np.absolute(rx)
+envelope = lpf(rectified, FILTER_CUTOFF_FREQUENCY, SAMPLING_FREQUENCY)
 signal = []
-for bit in PAYLOAD:
-    signal += [bit]*SAMPLES_PER_BIT
+for sample in envelope:
+    if sample > ADC_THRESHOLD:
+        signal.append(1)
+    else:
+        signal.append(0)
 
-time = np.arange(0, len(PAYLOAD), 1/SAMPLES_PER_BIT)  
-
-CARRIER_AMPLITUDE       = 1
-CARRIER_FREQUENCY       = 5
-carrier =  CARRIER_AMPLITUDE * sin(2*PI*CARRIER_FREQUENCY*time)
-
-tx = carrier*signal
-
-# demodulation
-rx = tx
-
-rectified = absolute(rx)
-
-envelope = lpf(rectified, CARRIER_FREQUENCY, SAMPLES_PER_BIT)
-
-rcvd_signal = [0 if s<0.5 else 1 for s in envelope]
-
-signals_to_plot = {'Payload':signal,
-                   'Carrier':carrier,
-                   'Tx':tx,
-                   'Rx':rx,
-                   'Rectified':rectified,
-                   'Envelope':envelope,
-                   'Received Payload': rcvd_signal
+signals_to_plot = {
+                   'Rx': rx,
+                   'Rectified': rectified,
+                   'Envelope': envelope,
+                   'Signal': signal
                    }
-
 for name, sig in signals_to_plot.items():
-    plt.figure(dpi=150)
-    plt.title(name)
+    plt.figure(figsize = [6.4,1.6], dpi=150)
+    # plt.title(name, fontsize=6)
     plt.grid()
-    plt.plot(time, sig)
+    plt.plot(time, sig, label = name)
+    plt.legend(fontsize = 6, loc = 'lower right')
     plt.show()
+
+plt.figure(figsize = [6.4,1.6], dpi=150)
+plt.grid()
+plt.plot(time, signals_to_plot['Rx'], linewidth = 2.5, label = 'Rx')
+plt.plot(time, signals_to_plot['Rectified'], linewidth = 1, label = 'Rectified')
+plt.legend(fontsize = 6, loc = 'lower right')
+plt.show()
+
+plt.figure(figsize = [6.4,1.6], dpi=150)
+plt.grid()
+plt.plot(time, signals_to_plot['Rectified'], linewidth = 1.5, label = 'Rectified')
+plt.plot(time, signals_to_plot['Envelope'], linewidth = 1, label = 'Envelope')
+plt.legend(fontsize = 6, loc = 'lower right')
+plt.show()
+
+plt.figure(figsize = [6.4,1.6], dpi=150)
+plt.grid()
+plt.plot(time, signals_to_plot['Envelope'], linewidth = 1.5, label = 'Envelope')
+plt.plot(time, signals_to_plot['Signal'], linewidth = 1, label = 'Signal')
+plt.legend(fontsize = 6, loc = 'lower right')
+plt.show()
+
+plt.figure(figsize = [6.4,1.6], dpi=150)
+plt.grid()
+plt.plot(time, signals_to_plot['Rx'], linewidth = 3, label = 'Rx')
+plt.plot(time, signals_to_plot['Rectified'], linewidth = 1, label = 'Rectified')
+plt.plot(time, signals_to_plot['Envelope'], linewidth = 1, label = 'Envelope')
+plt.plot(time, signals_to_plot['Signal'], linewidth = 1, label = 'Signal')
+plt.legend(fontsize = 6, loc = 'lower right')
+plt.show()
--- examples_py/AM_OOK_modulation.py
+++ examples_py/AM_OOK_modulation.py
@@ -1,30 +1,25 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from numpy import sin
 from numpy import pi as PI
 
-PAYLOAD                 = [0,1,0,1,1,0,0,1,0,1,1,1]
-SAMPLES_PER_BIT         = 1000
+PAYLOAD                     = [1,0,1,0,1,1]
+SAMPLES_PER_BIT             = 1000
+CARRIER_PERIODS_PER_BITS    = 5
+time = np.linspace(0, len(PAYLOAD), len(PAYLOAD)*SAMPLES_PER_BIT)
 signal = []
 for bit in PAYLOAD:
     signal += [bit]*SAMPLES_PER_BIT
-
-time = np.arange(0, len(PAYLOAD), 1/SAMPLES_PER_BIT)  
-
-CARRIER_AMPLITUDE       = 1
-CARRIER_FREQUENCY       = 5
-carrier =  CARRIER_AMPLITUDE * sin(2*PI*CARRIER_FREQUENCY*time)
-
+carrier = np.sin(2*PI*CARRIER_PERIODS_PER_BITS*time)
 tx = carrier*signal
+np.save('AM_OOK_TxSignal', tx)
 
 signals_to_plot = {'Payload':signal,
                    'Carrier':carrier,
                    'Tx':tx
                    }
-
 for name, sig in signals_to_plot.items():
-    plt.figure(dpi=150)
-    plt.title(name)
+    plt.figure(figsize = [6.4,1.6], dpi=150)
+    # plt.title(name, fontsize=6)
     plt.grid()
     plt.plot(time, sig)
     plt.show()
