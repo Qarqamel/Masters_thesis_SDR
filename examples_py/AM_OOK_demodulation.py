2c0ca75f4b12b591e487485447fe7a24817eec49
Adam Mundala <adam.mundala@gmail.com>
2023-05-25 05:26:38 +0200

    Plot formatting changed for AM OOK

--- examples_py/AM_OOK_demodulation.py
+++ examples_py/AM_OOK_demodulation.py
@@ -6,10 +6,11 @@ def lpf(samples, cutoff_freq, sampling_freq):
     b, a = scipy.signal.butter(2, 2*cutoff_freq/sampling_freq, btype='lowpass')
     return scipy.signal.filtfilt(b,a,samples)
 
-TIME                        = 6
+TIME                        = 5
 SAMPLING_FREQUENCY          = 1000
-FILTER_CUTOFF_FREQUENCY     = 5
+FILTER_CUTOFF_FREQUENCY     = 3
 ADC_THRESHOLD               = 0.5
+
 time = np.linspace(0, TIME, TIME*SAMPLING_FREQUENCY)
 rx = np.load('AM_OOK_TxSignal.npy')
 rectified = np.absolute(rx)
@@ -27,40 +28,41 @@ signals_to_plot = {
                    'Envelope': envelope,
                    'Signal': signal
                    }
-for name, sig in signals_to_plot.items():
-    plt.figure(figsize = [6.4,1.6], dpi=150)
-    # plt.title(name, fontsize=6)
-    plt.grid()
-    plt.plot(time, sig, label = name)
-    plt.legend(fontsize = 6, loc = 'lower right')
-    plt.show()
 
-plt.figure(figsize = [6.4,1.6], dpi=150)
+plt.rc('font', size=8)
+plt.rc('axes', titlesize=8)
+plt.rc('axes', labelsize=8)
+plt.rc('xtick', labelsize=8)
+plt.rc('ytick', labelsize=8)
+plt.rc('legend', fontsize=8)
+plt.rc('figure', titlesize=8)
+
+plt.figure(figsize = [6.4,1], dpi=150, facecolor='#FAF4F6')
+ax = plt.axes()
+ax.set_facecolor("#FAF4F6")
 plt.grid()
+plt.title('Rx/Rectified')
 plt.plot(time, signals_to_plot['Rx'], linewidth = 2.5, label = 'Rx')
 plt.plot(time, signals_to_plot['Rectified'], linewidth = 1, label = 'Rectified')
-plt.legend(fontsize = 6, loc = 'lower right')
+plt.legend(loc = 'lower right', ncol=2, bbox_to_anchor=(1, -0.6))
 plt.show()
 
-plt.figure(figsize = [6.4,1.6], dpi=150)
+plt.figure(figsize = [6.4,1], dpi=150, facecolor='#FAF4F6')
+ax = plt.axes()
+ax.set_facecolor("#FAF4F6")
 plt.grid()
+plt.title('Rectified/Envelope')
 plt.plot(time, signals_to_plot['Rectified'], linewidth = 1.5, label = 'Rectified')
 plt.plot(time, signals_to_plot['Envelope'], linewidth = 1, label = 'Envelope')
-plt.legend(fontsize = 6, loc = 'lower right')
+plt.legend(loc = 'lower right', ncol=2, bbox_to_anchor=(1, -0.6))
 plt.show()
 
-plt.figure(figsize = [6.4,1.6], dpi=150)
+plt.figure(figsize = [6.4,1], dpi=150, facecolor='#FAF4F6')
+ax = plt.axes()
+ax.set_facecolor("#FAF4F6")
 plt.grid()
+plt.title('Envelope/Signal')
 plt.plot(time, signals_to_plot['Envelope'], linewidth = 1.5, label = 'Envelope')
 plt.plot(time, signals_to_plot['Signal'], linewidth = 1, label = 'Signal')
-plt.legend(fontsize = 6, loc = 'lower right')
-plt.show()
-
-plt.figure(figsize = [6.4,1.6], dpi=150)
-plt.grid()
-plt.plot(time, signals_to_plot['Rx'], linewidth = 3, label = 'Rx')
-plt.plot(time, signals_to_plot['Rectified'], linewidth = 1, label = 'Rectified')
-plt.plot(time, signals_to_plot['Envelope'], linewidth = 1, label = 'Envelope')
-plt.plot(time, signals_to_plot['Signal'], linewidth = 1, label = 'Signal')
-plt.legend(fontsize = 6, loc = 'lower right')
+plt.legend(loc = 'lower right', ncol=2, bbox_to_anchor=(1, -0.6))
 plt.show()
--- examples_py/AM_OOK_modulation.py
+++ examples_py/AM_OOK_modulation.py
@@ -2,9 +2,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numpy import pi as PI
 
-PAYLOAD                     = [1,0,1,0,1,1]
+PAYLOAD                     = [0,1,1,0,1]
 SAMPLES_PER_BIT             = 1000
-CARRIER_PERIODS_PER_BITS    = 5
+CARRIER_PERIODS_PER_BITS    = 3
+
 time = np.linspace(0, len(PAYLOAD), len(PAYLOAD)*SAMPLES_PER_BIT)
 signal = []
 for bit in PAYLOAD:
@@ -13,13 +14,24 @@ carrier = np.sin(2*PI*CARRIER_PERIODS_PER_BITS*time)
 tx = carrier*signal
 np.save('AM_OOK_TxSignal', tx)
 
+plt.rc('font', size=8)
+plt.rc('axes', titlesize=8)
+plt.rc('axes', labelsize=8)
+plt.rc('xtick', labelsize=8)
+plt.rc('ytick', labelsize=8)
+plt.rc('legend', fontsize=8)
+plt.rc('figure', titlesize=8)
+
 signals_to_plot = {'Payload':signal,
                    'Carrier':carrier,
                    'Tx':tx
                    }
 for name, sig in signals_to_plot.items():
-    plt.figure(figsize = [6.4,1.6], dpi=150)
-    # plt.title(name, fontsize=6)
+    plt.figure(figsize = [6.4,1], dpi=150, facecolor='#FAF4F6') #F4E8EB
+    ax = plt.axes()
+    ax.set_facecolor("#FAF4F6")
     plt.grid()
-    plt.plot(time, sig)
+    plt.title(name)
+    plt.plot(time, sig, label = name)
+    plt.legend(loc = 'lower right', bbox_to_anchor=(1, -0.6))
     plt.show()
